5640083473957a783d2fcaf85de6944bb39d9d16
David Chan <chenyanliang@mail.btc38.com>
2023-01-10 15:16:36 +0800

    first commit

---
+++ lib/mask_guide_navigator_observer.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:mask_guide/mask_controller.dart';
+
+class MaskGuideNavigatorObserver extends NavigatorObserver {
+
+  @override
+  void didPop(Route<dynamic> route, Route<dynamic>? previousRoute) async {
+    try {
+      final MaskController controller = MaskController();
+      if (controller.canPop) {
+        controller.done.value = true;
+      }
+    } catch(_) {}
+  }
+}
+
